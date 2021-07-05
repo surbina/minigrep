b5e880639d4916bde078849348019cd2368ae3e5
Sebastian Urbina <sebita.urbina@gmail.com>
2021-07-04 23:16:33 -0300

    add case insensitive search

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Search term {}", config.query);
-    println!("File {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
